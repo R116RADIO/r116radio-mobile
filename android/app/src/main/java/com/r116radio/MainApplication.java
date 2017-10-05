69a5c5c74ae4e8590e95232786b70d20a53f8e9b
bdavid68 <b.david6008@gmail.com>
2017-10-05 18:07:07 +1000

    auto play, enable landscape mode

--- android/app/src/main/java/com/r116radio/MainApplication.java
+++ android/app/src/main/java/com/r116radio/MainApplication.java
@@ -1,12 +1,6 @@
 package com.r116radio;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.util.Log;
-import android.view.WindowManager;
-import android.content.Intent;
+import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
@@ -14,48 +8,41 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.reactnativenavigation.NavigationApplication;
-import com.reactnativenavigation.controllers.ActivityCallbacks;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.audioStreaming.ReactNativeAudioStreamingPackage;
-
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends NavigationApplication {
+import com.github.yamill.orientation.OrientationPackage;
+import com.audioStreaming.ReactNativeAudioStreamingPackage;
+import com.audioStreaming.ReactNativeAudioStreamingPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 
-  @Override
-    public boolean isDebug() {
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
-    @Nullable
     @Override
-    public List<ReactPackage> createAdditionalReactPackages() {
+    protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
           new RNFetchBlobPackage(),
-          new LinearGradientPackage(),
-          new RNDeviceInfo(),
+          new OrientationPackage(),
           new ReactNativeAudioStreamingPackage()
       );
     }
+  };
 
-    @Override
-    public void onCreate() {
-      super.onCreate();
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
 
-      SoLoader.init(this, /* native exopackage */ false);
-
-      final NavigationApplication sharedInstance = this;
-
-      setActivityCallbacks(new ActivityCallbacks() {
-        @Override
-        public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-          super.onActivityCreated(activity, savedInstanceState);
-          activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
-        }
-      });
-    }
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }
--- src/types/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * @flow
- * @providesModule AppTypes
- */
-
